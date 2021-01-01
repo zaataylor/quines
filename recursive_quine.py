3086c7f6d8219410fff5c21eb211ae8b3c738dfa
Zachary A. Taylor <ztaylor45685@gmail.com>
2021-01-01 11:35:48 -0500

    Add clarifying comment about recursion base case

--- recursive_quine.py
+++ recursive_quine.py
@@ -11,7 +11,9 @@ def recursive_quine(content):
 	if end != -1:
 		return content[start:end+1] + recursive_quine(content[end+1:])
 	else:
-		# no newline at end of file
+		# Case: we've read in the last newline of the file already,
+		# so end will just equal -1. Thus, we should return everything
+		# after the starting position.
 		return content[start:]
 
 if __name__ == "__main__":
